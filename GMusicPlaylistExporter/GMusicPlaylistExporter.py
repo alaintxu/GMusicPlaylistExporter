baccd72c8765ff5cd7ea6f1b7faee8c3f2cd7354
Alain Perez <aperez@mondragon.edu>
2013-05-15 14:03:20 +0100

    - File copy in a new thread. - Working if the audio files are in /data/data or /sdcard/Android/data - Cancel button does not work.

--- GMusicPlaylistExporter/GMusicPlaylistExporter.py
+++ GMusicPlaylistExporter/GMusicPlaylistExporter.py
@@ -6,27 +6,31 @@ Created on May 2, 2013
 '''
 import sys
 from PyQt4 import QtGui
+from PyQt4 import QtCore
 import sqlite3 as lite
-import commands
+import subprocess
+
 class GMPE(object):
     musicdbpath     =   "/data/data/com.google.android.music/databases/music.db"
     musicfolder     =   "/data/data/com.google.android.music/files/music"
+    musicfolder2     =   "/sdcard/Android/data/com.google.android.music/files/music"
     playlists       =   {}
     def __init__(self,androidSdkPath,exportPath):
         self.androidSdkPath =   androidSdkPath
         self.exportPath     =   exportPath
         self.playlists      =   {}
+        self.setAdbRoot()
         pass
-
+    def setExportPath(self,exportPath):
+        self.exportPath =   exportPath
+    def setAdbRoot(self):
+        subprocess.call(['%s/platform-tools/adb' % (self.androidSdkPath),'root'])
+        pass
     def getMusicDB(self):
-        command =   '%s/platform-tools/adb pull %s' % (self.androidSdkPath,self.musicdbpath);
-        output = commands.getoutput(command)
-        print output
+        subprocess.call(["%s/platform-tools/adb"%(self.androidSdkPath),"pull",self.musicdbpath])
         pass
     def remMusicDB(self):
-        command =   'rm music.db';
-        output = commands.getoutput(command)
-        print output
+        subprocess.call(["rm","music.db"])
         pass
     def getPlaylistsFromMusicDB(self):
         try:
@@ -59,21 +63,48 @@ class GMPE(object):
                 song['id']          =   row[5]
                 self.playlists[row[0]].append(song)
             return self.playlists
-        except Exception as e:
+        except Exception:
             return None
 
     def createDirectory(self,path):
-        command =   'mkdir "%s"' % (path)
-        output = commands.getoutput(command)
-        print output
+        subprocess.call(["mkdir",'%s' % (path)])
         pass
     def copySong(self,plPath,song,i):
-        command =   '%s/platform-tools/adb pull %s/%d.mp3 "%s/%d-%s.mp3"' % (self.androidSdkPath,self.musicfolder,song['id'],plPath,i,song['title']);
-        output = commands.getoutput(command)
-        print output
-
+        output  =   subprocess.call(
+             ["%s/platform-tools/adb" % (self.androidSdkPath),
+             "pull",
+             "%s/%d.mp3" % (self.musicfolder,song['id']),
+             '%s/%d-%s.mp3' % (plPath,i,song['title'])]
+        )
+        #command =   '%s/platform-tools/adb pull %s/%d.mp3 "%s/%d-%s.mp3"' % (self.androidSdkPath,self.musicfolder,song['id'],plPath,i,song['title']);
+        #output = commands.getoutput(command)
+        if output==1 and not self.musicfolder==self.musicfolder2:
+            self.musicfolder    =   self.musicfolder2
+            print "changed device music folder"
+            self.copySong(plPath,song,i)
+class ExportThread(QtCore.QThread):
+    def __init__(self,parent):
+        QtCore.QThread.__init__(self, parent)
+    def setPlaylists(self,playlists):
+        self.playlists  =   playlists
+    def setSelected(self,selected):
+        self.selected   =   selected
+    def setGMPE(self,gmpe):
+        self.gmpe       =   gmpe
+    def run (self):
+        for plname in self.selected:
+            playlist    =   self.playlists[plname]
+            self.emit(QtCore.SIGNAL("copyNewPlaylist"))
+            playlistPath    =  "%s/%s" % (self.gmpe.exportPath,plname) 
+            self.gmpe.createDirectory(playlistPath)
+            i   =   1
+            for song in playlist:
+                self.emit(QtCore.SIGNAL("copyNewSong"))
+                self.gmpe.copySong(playlistPath,song,i)
+                i+=1
+            self.emit(QtCore.SIGNAL('allSongsFinished'))
+        self.emit(QtCore.SIGNAL('allFinished'))
 class UserInterface(QtGui.QMainWindow):
-    
     androidSdkPath  =   "Select android SDK folder"
     exportPath      =   "Select export folder"
     gmpe            =   None
@@ -82,6 +113,16 @@ class UserInterface(QtGui.QMainWindow):
         super(UserInterface, self).__init__()
         
         self.initUI()
+        
+        self.thread = ExportThread(self)
+        self.connect(self.thread, QtCore.SIGNAL('copyNewPlaylist'),
+                     self.handleCopyNewPlaylist)
+        self.connect(self.thread, QtCore.SIGNAL('copyNewSong'),
+                     self.handleCopyNewSong)
+        self.connect(self.thread, QtCore.SIGNAL('allSongsFinished'),
+                     self.handleAllSongsFinished)
+        self.connect(self.thread, QtCore.SIGNAL('allFinished'),
+                     self.handleAllFinished)
         
     def initUI(self):
         changeSDK = QtGui.QAction('Change SDK folder', self)
@@ -131,6 +172,13 @@ class UserInterface(QtGui.QMainWindow):
         self.exportBtn.clicked.connect(self.export_playlists)
         self.exportBtn.move(15, 345)
         
+        self.cancelBtn = QtGui.QPushButton('Cancel exporting', self)
+        self.cancelBtn.setEnabled(0)
+        self.cancelBtn.setToolTip('Cancels exporting thread')
+        self.cancelBtn.resize(self.cancelBtn.sizeHint())
+        self.cancelBtn.clicked.connect(self.cancel_exporting)
+        self.cancelBtn.move(200, 345)
+        
         self.pllabel = QtGui.QLabel(self)
         self.pllabel.setText("Waiting")
         self.pllabel.setGeometry(15,375,450,25)
@@ -172,6 +220,7 @@ class UserInterface(QtGui.QMainWindow):
         self.load_playlists()
     def changeExportFolder(self):
         self.exportPath = QtGui.QFileDialog.getExistingDirectory(self, 'Select Export folder')
+        self.gmpe.setExportPath(self.exportPath)
         self.exportlabel.setText(self.exportPath)
         self.exportBtn.setEnabled(1);
     def load_playlists(self):
@@ -186,39 +235,51 @@ class UserInterface(QtGui.QMainWindow):
             self.list.show()
         else:
             QtGui.QMessageBox.question(self,'Error','Device not found or no playlist available',QtGui.QMessageBox.Ok)
-            
     def export_playlists(self):
-        ret={}
-        ret['ok']   =   0
-        ret['wrong']=   0
-        nofplaylists    =   len(self.list.selectedItems())
-        i_pl        =   1
-        
-        selected    =   []
+        self.selected    =   []
         for item in self.list.selectedItems():
-            selected.append(item.text())
-            
-        for plname,playlist in self.playlists.items():
-            if plname in selected:
-                self.pllabel.setText("%d/%d - %s" % (i_pl,nofplaylists,plname))
-                self.plpbar.setValue(100*(i_pl-1)/nofplaylists)
-                playlistPath    =  "%s/%s" % (self.gmpe.exportPath,plname) 
-                self.gmpe.createDirectory(playlistPath)
-                nofsongs    =   len(playlist)
-                i   =   1
-                for song in playlist:
-                    self.slabel.setText("%d/%d - %s" % (i,nofsongs,song['title']))
-                    self.spbar.setValue(100*(i-1)/nofsongs)
-                    self.gmpe.copySong(playlistPath,song,i)
-                    i+=1
-                self.spbar.setValue(100)
-                i_pl+=1
-        
-        self.plpbar.setValue(100)
-        self.pllabel.setText("Finished")
+            self.selected.append(str(item.text()))
+        self.i=0
+        self.j=0
+        self.thread.setSelected(self.selected)
+        self.thread.setPlaylists(self.playlists)
+        self.thread.setGMPE(self.gmpe)
+        self.cancelBtn.setEnabled(1)
+        self.exportBtn.setEnabled(0)
+        self.thread.quit()
+        self.thread.start()
+    def cancel_exporting(self):
+        self.cancelBtn.setEnabled(0)
+        self.thread.quit()
+        self.pllabel("Cancelled")
+        self.plpbar.setValue(0)
+        self.slabel("Cancelled")
+        self.spbar.setValue(0)
+        self.exportBtn.setEnabled(1)
+    def handleCopyNewPlaylist(self):
+        currentPlaylistName =   self.selected[self.i]
+        nofplaylists        =   len(self.selected)
+        self.pllabel.setText("%s %d/%d" % (currentPlaylistName,self.i+1,nofplaylists))
+        self.plpbar.setValue(100*self.i/nofplaylists)
+    def handleCopyNewSong(self):
+        currentPlaylistName =   self.selected[self.i]
+        currentPlaylist     =   self.playlists[currentPlaylistName.encode('utf8')]
+        nofsongs            =   len(currentPlaylist)
+        song                =   currentPlaylist[self.j]
+        self.slabel.setText("%s %d/%d" % (song['title'],self.j+1,len(currentPlaylist)))
+        self.spbar.setValue(100*self.j/nofsongs)
+        self.j+=1
+    def handleAllSongsFinished(self):
         self.slabel.setText("Finished")
-        pass
-
+        self.spbar.setValue(100)
+        self.j = 0
+        self.i+=1
+    def handleAllFinished(self):
+        self.pllabel.setText("Finished")
+        self.plpbar.setValue(100)
+        self.cancelBtn.setEnabled(0)
+        self.exportBtn.setEnabled(1)
+        self.i = 0
 def main():
     app = QtGui.QApplication(sys.argv)
     ex = UserInterface()
