5c1d43b58c8cca50a1dc91573d0dc83c6d76c7d9
Alain Perez <alain.perez@outlook.com>
2016-02-19 17:09:53 +0100

    indent modification and DB error patched

--- GMusicPlaylistExporter/GMusicPlaylistExporter.py
+++ GMusicPlaylistExporter/GMusicPlaylistExporter.py
@@ -18,106 +18,109 @@ class GMPE(object):
     musicfolder     =   "/storage/external_SD/Android/data/com.google.android.music/files/music"
     musicfolder2     =   "/sdcard/Android/data/com.google.android.music/files/music"
     playlists       =   {}
-    special_characters	=	'[:\/?]'
-    GMPEdb_name		=	r"gmpe.db"
+    special_characters    =    '[:\/?]'
+    GMPEdb_name        =    "gmpe.db"
     def __init__(self):
-    	self.androidSdkPath	=	"androidSdkPath"
-    	self.exportPath		=	"exportPath"
-    	GMPEdb			=	None
-    	try:
-    		query	=	'''
-    				SELECT
-    					name
-    				FROM
-    					sqlite_master
-    				WHERE
-    					type="table" AND 
-    					name="variable";'''
-    		GMPEdb		=	lite.connect(self.GMPEdb_name)
-            	cur = GMPEdb.cursor()    
-            	cur.execute(query)
-            	result = cur.fetchone()
-            	if result==None:
-            		query	=	'''
-            				CREATE TABLE
-            					variable
-             				(variable text primary key, value text)
-            				'''
-            		cur.execute(query)
-            	print result
+        self.androidSdkPath    =    "androidSdkPath"
+        self.exportPath        =    "exportPath"
+        GMPEdb            =    None
+        try:
+            query    =    '''
+                    SELECT
+                        name
+                    FROM
+                        sqlite_master
+                    WHERE
+                        type="table" AND 
+                        name="variable";'''
+            GMPEdb        =    lite.connect(self.GMPEdb_name)
+            cur = GMPEdb.cursor()    
+            cur.execute(query)
+            result = cur.fetchone()
+            if result==None:
+                query    =    '''
+                        CREATE TABLE
+                            variable
+                        (variable text primary key, value text)
+                        '''
+                cur.execute(query)
+                print "'variable' table created!"
+            print result
         except Exception as e:
-        	print e
-    	if GMPEdb:
-		GMPEdb.close()
-    		
-	self.androidSdkPath	=	self.getVariable("androidSdkPath")
-	self.exportPath		=	self.getVariable("exportPath")
+            print e
+        if GMPEdb:
+            GMPEdb.close()
+            
+        self.androidSdkPath    =    self.getVariable("androidSdkPath")
+        self.exportPath        =    self.getVariable("exportPath")
         self.playlists      =   {}
         pass
+
     def setAdbRoot(self):
-    	if self.androidSdkPath!="androidSdkPath":
-    		try:
-        		subprocess.call(['%s/platform-tools/adb' % (self.androidSdkPath),'root'])
-        	except Exception as e:
-        		print e
+        if self.androidSdkPath!="androidSdkPath":
+            try:
+                subprocess.call(['%s/platform-tools/adb' % (self.androidSdkPath),'root'])
+            except Exception as e:
+                print e
         pass
     def getMusicDB(self):
-    	self.androidSdkPath	=	self.getVariable("androidSdkPath");
-    	if self.androidSdkPath!="androidSdkPath":
-        	self.setAdbRoot()
-        	try:
-        		subprocess.call(["%s/platform-tools/adb"%(self.androidSdkPath),"pull",self.musicdbpath])
-        	except Exception as e:
-        		print e
+        self.androidSdkPath    =    self.getVariable("androidSdkPath");
+        if self.androidSdkPath!="androidSdkPath":
+            self.setAdbRoot()
+            try:
+                subprocess.call(["%s/platform-tools/adb"%(self.androidSdkPath),"pull",self.musicdbpath])
+            except Exception as e:
+                print e
         pass
     def remMusicDB(self):
         subprocess.call(["rm","music.db"])
         pass
     def getVariable(self,variable):
-    	GMPEdb	=	None
-    	ret	=	variable
-    	try:
-    		query	=	'''
-    				SELECT
-    					value
-    				FROM
-    					variable
-    				WHERE
-    					variable="%s"''' % (variable)
-    		GMPEdb		=	lite.connect(self.GMPEdb_name)
-            	cur = GMPEdb.cursor()    
-            	cur.execute(query)
-            	ret = cur.fetchone()
-            	print "getVariable(%s)" % (variable)
-            	if ret==None:
-            		ret=variable
-            	else:
-            		ret=ret[0]
+        GMPEdb    =    None
+        ret    =    variable
+        try:
+            query    =    '''
+                    SELECT
+                        value
+                    FROM
+                        variable
+                    WHERE
+                        variable="%s"''' % (variable)
+            GMPEdb        =    lite.connect(self.GMPEdb_name)
+            cur = GMPEdb.cursor()    
+            cur.execute(query)
+            ret = cur.fetchone()
+            print "getVariable(%s)" % (variable)
+            if ret==None:
+                ret=variable
+            else:
+                ret=ret[0]
         except Exception as e:
-        	print e
-        	ret	=	variable
-    	if GMPEdb:
-		GMPEdb.close()
-	print ret
-	return ret
+            print e
+            ret    =    variable
+        if GMPEdb:
+            GMPEdb.close()
+        print ret
+        return ret
     def setVariable(self,variable,value):
-    	GMPEdb	=	None
-    	try:
-            	print "setVariable(%s,%s)" % (variable,value)
-    		GMPEdb		=	lite.connect(self.GMPEdb_name)
-            	cur = GMPEdb.cursor()
-            	item	=	[variable,value]
-            	result	=	cur.execute("INSERT INTO variable (variable, value) VALUES(?,?)",item)
-            	
-            	print result
-    	except Exception as e:
-    		print e
-    	
-    	if GMPEdb:
-		GMPEdb.close()
-    		
+        GMPEdb    =    None
+        try:
+            print "setVariable(%s,%s)" % (variable,value)
+            GMPEdb        =    lite.connect(self.GMPEdb_name)
+            cur = GMPEdb.cursor()
+            item    =    [str(variable),str(value)]
+            print item
+            result    =    cur.execute("INSERT INTO variable (variable, value) VALUES(?,?)",item)
+            GMPEdb.commit();
+            print "result: "+str(result)
+        except Exception as e:
+            print "error: "+str(e)
+        
+        if GMPEdb:
+            GMPEdb.close()
+            
     def getPlaylistsFromMusicDB(self):
-        con	=	None
+        con    =    None
         self.addThumbsUp()
         try:
             query   =   '''SELECT
@@ -129,8 +132,8 @@ class GMPE(object):
                             m.id,
                             m.year,
                             m.Rating
-			    FROM music as m
-			    JOIN listitems as li ON li.MusicId = m.id
+                FROM music as m
+                JOIN listitems as li ON li.MusicId = m.id
                         JOIN lists as l ON li.listid = l.id
                         WHERE l.ListType=0
                         ORDER BY l.id, m.id;'''
@@ -152,25 +155,25 @@ class GMPE(object):
                 song['tracknumber'] = row[3]
                 song['title']       = re.sub(self.special_characters, '', row[4])
                 song['id']          = row[5]
-                song['year']	    = row[6]
+                song['year']        = row[6]
                 self.playlists[row[0]].append(song)
-    	    if con:
-        	con.close()
+            if con:
+                con.close()
             return self.playlists
         except Exception as e:
         
-    	    if con:
-        	con.close()
+            if con:
+                con.close()
             print "Unable to get playlist from music.db"
             print e
             return None
             
     def addThumbsUp(self):
-    	con	=	None
-    	try:
-    		
-    	    self.playlists[u'ThumbsUp']	=	[]
-    	    query   =   '''SELECT
+        con    =    None
+        try:
+            
+            self.playlists[u'ThumbsUp']    =    []
+            query   =   '''SELECT
                             m.albumartist,
                             m.album,
                             m.TrackNumber,
@@ -178,7 +181,7 @@ class GMPE(object):
                             m.id,
                             m.year,
                             m.Rating
-			    FROM music as m
+                FROM music as m
                         WHERE m.Rating=5
                         ORDER BY m.albumartist, m.year,m.album,m.TrackNumber;'''
             con = lite.connect('music.db')
@@ -188,28 +191,28 @@ class GMPE(object):
             
         
             for row in rows:
-            	song    =   {}
-            	
-            	song['albumartist'] = re.sub(self.special_characters, '', row[0])
+                song    =   {}
+                
+                song['albumartist'] = re.sub(self.special_characters, '', row[0])
                 song['album']       = re.sub(self.special_characters, '', row[1])
                 song['tracknumber'] = row[2]
                 song['title']       = re.sub(self.special_characters, '', row[3])
                 song['id']          = row[4]
-                song['year']	    = row[5]
+                song['year']        = row[5]
                 self.playlists[u'ThumbsUp'].append(song)
-    	    if con:
-        	con.close()
-    	except Exception as e:
-    	    print "Unable to add ThumbsUp playlist"
-    	    if con:
-        	con.close()
+            if con:
+                con.close()
+        except Exception as e:
+            print "Unable to add ThumbsUp playlist"
+            if con:
+                con.close()
 
     def createDirectory(self,path):
         subprocess.call(["mkdir","%s" % (path)])
         pass
     def copySong(self,plPath,song,i):
-    	original_song_path	= "%s/%d.mp3" % (self.musicfolder,song['id'])
-    	song_path		= "%s/%d-%s.mp3" % (plPath,i,song['title'])
+        original_song_path    = "%s/%d.mp3" % (self.musicfolder,song['id'])
+        song_path        = "%s/%d-%s.mp3" % (plPath,i,song['title'])
         output  =   subprocess.call(
              ["%s/platform-tools/adb" % (self.androidSdkPath),
              "pull",
@@ -225,21 +228,21 @@ class GMPE(object):
                 print "changed device music folder"
                 self.copySong(plPath,song,i)
     def addID3Tag(self,song_path,song):
-    	print song_path
-    	try:
-	    	tags = ID3()
-	    	tags.add(TIT2(encoding=3, text=u"An example"))
-		tags.save(song_path)
+        print song_path
+        try:
+            tags = ID3()
+            tags.add(TIT2(encoding=3, text=u"An example"))
+            tags.save(song_path)
 
-	    	audio = EasyID3(song_path)
-		audio["title"]		= song['title']
-		audio["album"]		= song['album']
-		audio["artist"]		= song['albumartist']
-		audio["date"]		= unicode(song['year'])
-		audio["tracknumber"]	= unicode(song['tracknumber'])
-		audio.save()
-	except Exception as e:
-            print e
+            audio = EasyID3(song_path)
+            audio["title"]        = song['title']
+            audio["album"]        = song['album']
+            audio["artist"]        = song['albumartist']
+            audio["date"]        = unicode(song['year'])
+            audio["tracknumber"]    = unicode(song['tracknumber'])
+            audio.save()
+        except Exception as e:
+                print e
 class ExportThread(QtCore.QThread):
     def __init__(self,parent):
         QtCore.QThread.__init__(self, parent)
@@ -319,7 +322,7 @@ class UserInterface(QtGui.QMainWindow):
         self.exportBrowseBtn.clicked.connect(self.changeExportFolder)
         self.exportBrowseBtn.move(230, 40)
         
-        self.reloadPlaylistsBtn	=	QtGui.QPushButton('Reload Playlists',self)
+        self.reloadPlaylistsBtn    =    QtGui.QPushButton('Reload Playlists',self)
         self.reloadPlaylistsBtn.resize(self.reloadPlaylistsBtn.sizeHint())
         self.reloadPlaylistsBtn.clicked.connect(self.reload_playlists)
         self.reloadPlaylistsBtn.move(180, 70)
@@ -385,35 +388,35 @@ class UserInterface(QtGui.QMainWindow):
         self.sdklabel.setText(androidSdkPath)
         self.load_playlists()
     def changeExportFolder(self):
-    	exportPath	=	QtGui.QFileDialog.getExistingDirectory(self, 'Select Export folder')
-    	self.gmpe.setVariable("exportPath",exportPath)
+        exportPath    =    QtGui.QFileDialog.getExistingDirectory(self, 'Select Export folder')
+        self.gmpe.setVariable("exportPath",exportPath)
         self.exportlabel.setText(exportPath)
         self.exportBtn.setEnabled(1)
     def reload_playlists(self):
-    	self.load_playlists()
+        self.load_playlists()
     def load_playlists(self):
-    	androidSdkPath	=	self.gmpe.getVariable("androidSdkPath")
-    	if androidSdkPath!="androidSdkPath":
+        androidSdkPath    =    self.gmpe.getVariable("androidSdkPath")
+        if androidSdkPath!="androidSdkPath":
 
-    		# Create and display the splash screen
-    		splash_pix = QtGui.QPixmap('loading.gif')
-    		splash = QtGui.QSplashScreen(splash_pix, QtCore.Qt.WindowStaysOnTopHint)
-    		splash.setMask(splash_pix.mask())
-    		splash.show()
+            # Create and display the splash screen
+            splash_pix = QtGui.QPixmap('loading.gif')
+            splash = QtGui.QSplashScreen(splash_pix, QtCore.Qt.WindowStaysOnTopHint)
+            splash.setMask(splash_pix.mask())
+            splash.show()
 
-    		
-		self.gmpe.getMusicDB()
-		self.playlists   =   self.gmpe.getPlaylistsFromMusicDB()
-		self.list.clear()
-		if not self.playlists==None:
-		    for plname,playlist in self.playlists.items():
-		    	print "Playlist:%s\nNofSongs:%d\n\n" % (plname,len(playlist))
-		        item    =   QtGui.QListWidgetItem(plname)
-		        self.list.addItem(item)
-		    self.list.show()
-		else:
-		    QtGui.QMessageBox.question(self,'Error','Device not found or no playlist available',QtGui.QMessageBox.Ok)
-    		splash.finish(self)
+            
+            self.gmpe.getMusicDB()
+            self.playlists   =   self.gmpe.getPlaylistsFromMusicDB()
+            self.list.clear()
+            if not self.playlists==None:
+                for plname,playlist in self.playlists.items():
+                    print "Playlist:%s\nNofSongs:%d\n\n" % (plname,len(playlist))
+                    item    =   QtGui.QListWidgetItem(plname)
+                    self.list.addItem(item)
+                self.list.show()
+            else:
+                QtGui.QMessageBox.question(self,'Error','Device not found or no playlist available',QtGui.QMessageBox.Ok)
+                splash.finish(self)
     def export_playlists(self):
         self.selected    =   []
         for item in self.list.selectedItems():
